72b2e4efa295196dfac5f618103b109c89431eb0
shuangliang kolu <shuangkolu@hotmail.com>
2016-04-22 11:55:30 +0300

    Changed eventlistener.js. rotp from true/false to max resist given. Calculate total resistance.

--- js/eventlistener.js
+++ js/eventlistener.js
@@ -18,18 +18,21 @@ for( var x = 0; x < inputList.length; x++ ){
     inputList[ x ].addEventListener( 'change', function( event ){
         var name = this.getAttribute( 'name' );
         
-        // Checks for rise of the phoenix checkbox
+        // Checks for rise of the phoenix checkbox.
         if( name.localeCompare( 'rotp' ) == 0 ){
-            vars[ name ] = this.checked;
+            if( this.checked )
+                vars.rotp = 8;
+            else
+                vars.rotp = 0;
         }
-        // Checks for barbarism passive checkbox
+        // Checks for barbarism passive checkbox.
         else if( name.localeCompare( 'barbarism' ) == 0 ){
             if( this.checked )
                 vars.barbarism = 1;
             else
                 vars.barbarism = 0;
         }
-        // Everything else
+        // Everything else.
         else{
             vars[ name ] = Number( this.value );
         }
@@ -40,5 +43,33 @@ for( var x = 0; x < inputList.length; x++ ){
 
 
 function calculateResult(){
-    console.log( vars );
+    // Calculate total resistance and display it on html element.
+    // Last value is max resist given by purity multiplied by aura effective multiplier.
+    var resistance = vars.baseRes + vars.barbarism + vars.rotp + vars.purity + ( Math.floor( vars.purity * ( vars.aura / 100 ) ) );
+    document.getElementById( 'resistance' ).value = resistance;
+    
+    // TODO CALCS
+    // lifeDamage
+    
+    // TODO CALCS
+    // esDamage
+    
+    // TODO CALCS
+    // lifeReducedDamage
+    
+    // TODO CALCS
+    // esReducedDamage
+    
+    // TODO CALCS
+    // regenRequired
+    
+    // TODO CALCS
+    // lifeRequired
+    
+    // TODO CALCS
+    // esRequired
+    
+    // TODO CALCS
+    // result
+    
 }
